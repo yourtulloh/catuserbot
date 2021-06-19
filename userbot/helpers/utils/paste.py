b0ca769b0ab88abd9d5991c2697ce475759a2bf0
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-19 13:08:58 +0530

    Update paste.py

--- userbot/helpers/utils/paste.py
+++ userbot/helpers/utils/paste.py
@@ -6,6 +6,7 @@ from ...Config import Config
 from ...core.logger import logging
 
 LOGS = logging.getLogger("CatUserbot")
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
     "content-type": "application/json",
