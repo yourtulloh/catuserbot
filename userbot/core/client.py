bfec868d497b15f5f7491edb49e9ee2cf54e6859
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-24 00:54:00 +0530

    Update client.py

--- userbot/core/client.py
+++ userbot/core/client.py
@@ -17,7 +17,7 @@ from ..sql_helper.globals import gvarstatus
 from . import BOT_INFO, CMD_INFO, GRP_INFO, LOADED_CMDS, PLG_INFO
 from .cmdinfo import _format_about
 from .data import _sudousers_list, blacklist_chats_list, sudo_enabled_cmds
-from .events import MessageEdited, NewMessage
+from .events import *
 from .fasttelethon import download_file, upload_file
 from .logger import logging
 from .managers import edit_delete
@@ -297,3 +297,6 @@ CatUserBotClient.fast_upload_file = upload_file
 CatUserBotClient.reload = restart_script
 CatUserBotClient.get_msg_link = get_message_link
 CatUserBotClient.check_testcases = checking
+CatUserBotClient.send_message = send_message
+CatUserBotClient.send_file = send_file
+CatUserBotClient.edit_message = edit_message
