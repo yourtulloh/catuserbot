7fbd8daa1dc92ba6dda65828618958daba845fdf
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-24 02:12:24 +0530

    Update session.py

--- userbot/core/session.py
+++ userbot/core/session.py
@@ -2,7 +2,7 @@ import sys
 
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 from telethon.sessions import StringSession
-
+from telethon import TelegramClient
 from ..Config import Config
 from .client import CatUserBotClient
 
@@ -26,13 +26,34 @@ try:
         auto_reconnect=True,
         connection_retries=None,
     )
+    t_catub =TelegramClient(
+        session=session,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
 except Exception as e:
     print(f"STRING_SESSION - {str(e)}")
     sys.exit()
 
 
-catub.tgbot = tgbot = CatUserBotClient(
+catub.tgbot = CatUserBotClient(
     session="CatTgbot",
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    loop=loop,
+    app_version=__version__,
+    connection=ConnectionTcpAbridged,
+    auto_reconnect=True,
+    connection_retries=None,
+).start(bot_token=Config.TG_BOT_TOKEN)
+
+t_catub.tgbot = TelegramClient(
+    session="Cat_Tgbot",
     api_id=Config.APP_ID,
     api_hash=Config.API_HASH,
     loop=loop,
