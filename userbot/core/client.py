8325b6b0a624c27ce20dcc91394886f7129eae90
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2021-06-23 20:44:45 +0000

    pylint: auto fixes

--- userbot/core/client.py
+++ userbot/core/client.py
@@ -265,8 +265,6 @@ class CatUserBotClient(TelegramClient):
                             Config.PRIVATE_GROUP_BOT_API_ID, text, link_preview=False
                         )
 
-            from .session import tgbot
-
             if edited is True:
                 catub.tgbot.add_event_handler(func, events.MessageEdited(**kwargs))
             else:
