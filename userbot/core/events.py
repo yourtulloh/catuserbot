45f199a535e527d105eb73a6c690b5055b365b95
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2021-06-24 00:52:35 +0530

    Update events.py

--- userbot/core/events.py
+++ userbot/core/events.py
@@ -74,3 +74,45 @@ class MessageEdited(NewMessage):
 
     class Event(NewMessage.Event):
         pass
+
+async def send_message(client, **kwargs):
+    chatid = kwargs.get("entity",-100)
+    if str(chatid) == str(Config.BOTLOG_CHATID):
+        return await client.send_message(**kwargs)
+    msg = kwargs.get("message","")
+    if (Config.STRING_SESSION in msg) or (Config.APP_ID in msg) or (Config.API_HASH in msg) or (Config.TG_BOT_TOKEN in msg) or (Config.HEROKU_API_KEY and Config.HEROKU_API_KEY in msg) or (Config.SCREEN_SHOT_LAYER_ACCESS_KEY and Config.SCREEN_SHOT_LAYER_ACCESS_KEY in msg):
+        if BOTLOG:
+            kwargs["entity"] = Config.BOTLOG_CHATID
+            await client.send_message(**kwargs)
+        msg = "Sorry I can't send this information in public chats i will send it in Bot Log group check it from there"
+    kwargs["message"] = msg
+    kwargs["entity"] = chatid
+    return await client.send_message(**kwargs)
+
+async def send_file(client, **kwargs):
+    chatid = kwargs.get("entity",-100)
+    if str(chatid) == str(Config.BOTLOG_CHATID):
+        return await client.send_file(**kwargs)
+    msg = kwargs.get("caption","")
+    if (Config.STRING_SESSION in msg) or (Config.APP_ID in msg) or (Config.API_HASH in msg) or (Config.TG_BOT_TOKEN in msg) or (Config.HEROKU_API_KEY and Config.HEROKU_API_KEY in msg) or (Config.SCREEN_SHOT_LAYER_ACCESS_KEY and Config.SCREEN_SHOT_LAYER_ACCESS_KEY in msg):
+        if BOTLOG:
+            kwargs["entity"] = Config.BOTLOG_CHATID
+            await client.send_file(**kwargs)
+        msg = "Sorry I can't send this information in public chats i will send it in Bot Log group check it from there"
+    kwargs["caption"] = msg
+    kwargs["entity"] = chatid
+    return await client.send_file(**kwargs)
+    
+async def edit_message(client, **kwargs):
+    chatid = kwargs.get("entity",-100)
+    if str(chatid) == str(Config.BOTLOG_CHATID):
+        return await client.edit_message(**kwargs)
+    msg = kwargs.get("message","")
+    if (Config.STRING_SESSION in msg) or (Config.APP_ID in msg) or (Config.API_HASH in msg) or (Config.TG_BOT_TOKEN in msg) or (Config.HEROKU_API_KEY and Config.HEROKU_API_KEY in msg) or (Config.SCREEN_SHOT_LAYER_ACCESS_KEY and Config.SCREEN_SHOT_LAYER_ACCESS_KEY in msg):
+        if BOTLOG:
+            kwargs["entity"] = Config.BOTLOG_CHATID
+            await client.edit_message(**kwargs)
+        msg = "Sorry I can't send this information in public chats i will send it in Bot Log group check it from there"
+    kwargs["message"] = msg
+    kwargs["entity"] = chatid
+    return await client.edit_message(**kwargs)
